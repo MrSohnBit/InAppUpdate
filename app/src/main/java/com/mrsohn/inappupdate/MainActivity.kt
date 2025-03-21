62c360c323d75b17d44622c3fbc8e2c5eb0d2f22
mrsohnHome <ssso.gooood@gmail.com>
2025-03-21 17:30:48 +0900

    앱 업데이트 기능 추가: 업데이트 체크, 강제/유연 업데이트 버튼, 진행률 표시 기능 추가
    Changes include:
    - Added a horizontal progress bar to display the download progress.
    - Added buttons for "Force Update" and "Flexible Update".
    - Added logic to check for updates and present options for force or flexible updates.
    - Added logic to display app version name and code.
    - Added logging to the view.
    - Added update listeners and install logic.
    - Added function to restart the app after update.
    - Modified layout to display updated elements.
    - Modified version text to display correctly.
    - Modified button text.
    - Changed the update check to display more log information.
    - Changed logic for checking latest version.
    - Added more log in the source code.

--- app/src/main/java/com/mrsohn/inappupdate/MainActivity.kt
+++ app/src/main/java/com/mrsohn/inappupdate/MainActivity.kt
@@ -3,29 +3,18 @@ package com.mrsohn.inappupdate
 import android.app.Activity
 import android.content.DialogInterface
 import android.content.Intent
-import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.launch
 import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.databinding.DataBindingUtil.setContentView
 import com.google.android.play.core.appupdate.AppUpdateInfo
 import com.google.android.play.core.appupdate.AppUpdateManager
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
@@ -34,19 +23,11 @@ import com.google.android.play.core.install.InstallStateUpdatedListener
 import com.google.android.play.core.install.model.AppUpdateType
 import com.google.android.play.core.install.model.InstallStatus
 import com.google.android.play.core.install.model.UpdateAvailability
-import com.google.android.play.core.ktx.requestAppUpdateInfo
-import com.google.android.play.core.ktx.updatePriority
 import com.mrsohn.inappupdate.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     private val TAG = javaClass.simpleName
 
-//    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     private val appUpdateManager: AppUpdateManager by lazy {
         AppUpdateManagerFactory.create(this)
@@ -84,17 +65,18 @@ class MainActivity : AppCompatActivity() {
         binding.updateBtn.setOnClickListener {
             binding.updateText.text = ""
             checkForUpdates()
+
+            binding.updateBtnLayout.visibility = View.GONE
         }
     }
 
     private val activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result: ActivityResult ->
+        Log.d(TAG, "inAppUpdate activityResultLauncher Update flow result code: OK")
 
         when (result.resultCode) {
             Activity.RESULT_OK -> {
                 Log.d(TAG, "Update flow result code: OK")
-                showDialog("업데이트완료", "업데이트가 완료되었습니다.", {_,_ ->
-                    restartApp()
-                }, null)
+//                restartApp()
             }
             Activity.RESULT_CANCELED -> {
                 Log.d(TAG, "Update flow result code: CANCELED")
@@ -110,6 +92,177 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+
+    fun restartApp() {
+        Handler(Looper.getMainLooper()).postDelayed({
+//            val packageManager = packageManager
+            val intent = packageManager.getLaunchIntentForPackage("com.mrsohn.inappupdate")
+            val componentName = intent!!.component
+            val mainIntent = Intent.makeRestartActivityTask(componentName)
+            startActivity(mainIntent)
+//            exitProcess(0)
+        }, 300)
+    }
+
+    private fun checkForUpdates(isForceUpdate: Boolean = false) {
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            var result =""
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                // Request the update.
+                binding.updateBtnLayout.visibility = View.VISIBLE
+
+                if (appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                    setResultTxt(true, result)
+                    binding.flexUpdateBtn.setOnClickListener {
+                        showUpdateAlert(appUpdateInfo, AppUpdateType.FLEXIBLE)
+                    }
+                }
+
+            } else {
+                binding.updateBtnLayout.visibility = View.VISIBLE
+            }
+
+            binding.flexUpdateBtn.visibility = if (appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)) View.VISIBLE else View.GONE
+            binding.forceUpdateBtn.setOnClickListener {
+                showUpdateAlert(appUpdateInfo, AppUpdateType.IMMEDIATE)
+            }
+
+            binding.flexUpdateBtn.setOnClickListener {
+                showUpdateAlert(appUpdateInfo, AppUpdateType.FLEXIBLE)
+            }
+
+            setAppUpdateInfoLog(appUpdateInfo)
+
+            if (isForceUpdate)
+                startUpdate(appUpdateInfo, AppUpdateType.IMMEDIATE)
+        }
+
+        appUpdateInfoTask.addOnFailureListener { exception ->
+            Log.e(TAG, "Failed to get app update info", exception)
+            // Exception 객체를 통해 실패 원인을 확인
+            val errorMessage = exception.message
+            val cause = exception.cause
+            Log.e(TAG, "Error message: $errorMessage")
+            Log.e(TAG, "Cause: $cause")
+
+            var result = "Error message: $errorMessage"
+            result += "\n"
+            result += "Cause: $cause"
+            setResultTxt(false, result)
+        }
+    }
+
+    fun setAppUpdateInfoLog(appUpdateInfo: AppUpdateInfo) {
+        var result = ""
+        val packageInfo = packageManager.getPackageInfo(packageName, 0)
+        val versionCode : Long = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                                    packageInfo.longVersionCode
+                                } else {
+                                    packageInfo.versionCode.toLong()
+                                }
+
+        if (versionCode >= appUpdateInfo.availableVersionCode()) {
+            result = "최신버전 입니다.\n\n"
+        }
+
+        result += "구글플레이 등록 버전코드 : ${appUpdateInfo.availableVersionCode()}\n\n"
+        result += "강제업데이트 가능 : ${appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)}\n"
+        result += "유연 업데이트 가능 : ${appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)}\n"
+        result += "updateAvailability() : ${appUpdateInfo.updateAvailability()}\n"
+
+        setResultTxt(true, result)
+    }
+
+    private fun showUpdateAlert(appUpdateInfo: AppUpdateInfo, appUpdateType: Int) {
+        showDialog("${if (appUpdateType == AppUpdateType.IMMEDIATE) "강제" else "선택"} 업데이트",
+            "업데이트 하시겠습니까?",
+            { _, _ ->
+                startUpdate(appUpdateInfo, appUpdateType)
+
+                binding.updateBtnLayout.visibility = View.GONE
+            },
+            { dialog, _ ->})
+    }
+
+    private fun setResultTxt(isSucceed: Boolean, resultText: String) {
+        val sb = StringBuilder()
+//        sb.append("Update flow result: ${if (isSucceed) "SUCCESS" else "FAILED"}")
+        if (!isSucceed)
+            sb.append("업데이트 정보 조회 실패\n\n")
+
+        sb.append(resultText)
+        binding.updateText.text = sb.toString()
+    }
+
+
+    private fun startUpdate(appUpdateInfo: AppUpdateInfo, appUpdateType: Int) {
+        appUpdateManager.startUpdateFlowForResult(
+            // Pass the intent that is returned by 'getAppUpdateInfo()'.
+            appUpdateInfo,
+            // an activity result launcher registered via registerForActivityResult
+            activityResultLauncher,
+            // Or pass 'AppUpdateType.FLEXIBLE' to newBuilder() for
+            // flexible updates.
+            AppUpdateOptions.newBuilder(appUpdateType)
+//            AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE)
+                .build())
+
+        setAppUpdateListener(appUpdateInfo, appUpdateType)
+    }
+
+    fun setAppUpdateListener(appUpdateInfo: AppUpdateInfo, appUpdateType: Int) {
+        // Create a listener to track request state updates.
+        val inAppUpdateListener = InstallStateUpdatedListener { state ->
+            // (Optional) Provide a download progress bar.
+
+            if (state.installStatus() == InstallStatus.DOWNLOADED) {
+//                popupSnackbarForCompleteUpdate()
+                Log.i(TAG, "InappUpdate Update: InstallStatus.DOWNLOADED, FLEXIBLE=${appUpdateType == AppUpdateType.FLEXIBLE}")
+                if (appUpdateType == AppUpdateType.FLEXIBLE) {
+                    showDialog("다운로드 완료",
+                        "다운로드가 완료되었습니다. 설치 하시겠습니까?",
+                        { _, _ ->
+//                            startUpdate(appUpdateInfo, AppUpdateType.IMMEDIATE)
+                            checkForUpdates(true)
+                        },
+                        { _, _ ->
+
+                        })
+
+                }
+                checkForUpdates(false)
+            } else if (state.installStatus() == InstallStatus.PENDING) {
+                Log.i(TAG, "InappUpdate Update: InstallStatus.PENDING")
+            } else if (state.installStatus() == InstallStatus.DOWNLOADING) {
+                val bytesDownloaded = state.bytesDownloaded()
+                val totalBytesToDownload = state.totalBytesToDownload()
+                Log.i(TAG, "InAppUpdate InstallStatus.DOWNLOADING: ${bytesDownloaded} / ${totalBytesToDownload}")
+                var progress = 0
+                if (bytesDownloaded != 0L)
+                    progress = ((bytesDownloaded/ totalBytesToDownload) * 100).toInt()
+
+                binding.progressBar.setProgress(progress, true)
+            } else if (state.installStatus() == InstallStatus.INSTALLING) {
+                Log.i(TAG, "InappUpdate Update: InstallStatus.INSTALLING")
+            } else if (state.installStatus() == InstallStatus.INSTALLED) {
+                Log.i(TAG, "InappUpdate Update: InstallStatus.INSTALLED")
+                // 앱을 재시작해줍니다.
+                restartApp()
+            } else if (state.installStatus() == InstallStatus.FAILED) {
+                Log.i(TAG, "InappUpdate Update: InstallStatus.FAILED")
+            } else if (state.installStatus() == InstallStatus.CANCELED) {
+                Log.i(TAG, "InappUpdate Update: InstallStatus.CANCELED")
+            } else {
+                Log.i(TAG, "InappUpdate Update: Else(${state.installStatus()})")
+            }
+        }
+
+        appUpdateManager.registerListener(inAppUpdateListener)
+    }
+
 
     fun showDialog(title: String, message: String, positiveClickListener: DialogInterface.OnClickListener?, negativeClickListener: DialogInterface.OnClickListener?) {
         AlertDialog.Builder(this@MainActivity).apply {
@@ -129,139 +282,4 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-
-    fun restartApp() {
-        Handler(Looper.getMainLooper()).postDelayed({
-            val packageManager = packageManager
-            val intent = packageManager.getLaunchIntentForPackage(packageName)
-            val componentName = intent!!.component
-            val mainIntent = Intent.makeRestartActivityTask(componentName)
-            startActivity(mainIntent)
-            exitProcess(0)
-        }, 300)
-    }
-
-    private fun checkForUpdates() {
-        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
-
-
-        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
-            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                // This example applies an immediate update. To apply a flexible update
-                // instead, pass in AppUpdateType.FLEXIBLE
-                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
-            ) {
-                // Request the update.
-
-                var result = "업데이트 가능\n"
-                "appUpdateInfo\n"
-                result += "updateAvailability() : ${appUpdateInfo.updateAvailability()}\n"
-                result += "isUpdateTypeAllowed(AppUpdateType.IMMEDIATE) : ${appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)}\n"
-                result += "isUpdateTypeAllowed(AppUpdateType.FLEXIBLE) : ${appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)}\n"
-                result += "installStatus() : ${appUpdateInfo.installStatus()}\n"
-                result += "updatePriority : ${appUpdateInfo.updatePriority}\n"
-                result += "availableVersionCode() : ${appUpdateInfo.availableVersionCode()}\n"
-                result += "clientVersionStalenessDays() : ${appUpdateInfo.clientVersionStalenessDays()}\n"
-
-                setResultTxt(true, result)
-
-                showDialog("업데이트",
-                    "업데이트 하시겠습니까?",
-                    { _, _ ->
-                    startUpdate(appUpdateInfo)
-                    },
-                    { dialog, _ ->})
-
-            } else {
-                Toast.makeText(this@MainActivity, "업데이트 불 가능(addOnSuccessListener)", Toast.LENGTH_SHORT).show()
-                var result = "업데이트 불 가능\n"
-                "appUpdateInfo\n"
-                result += "updateAvailability() : ${appUpdateInfo.updateAvailability()}\n"
-                result += "isUpdateTypeAllowed(AppUpdateType.IMMEDIATE) : ${appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)}\n"
-                result += "isUpdateTypeAllowed(AppUpdateType.FLEXIBLE) : ${appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)}\n"
-                result += "installStatus() : ${appUpdateInfo.installStatus()}\n"
-                result += "updatePriority : ${appUpdateInfo.updatePriority}\n"
-                result += "availableVersionCode() : ${appUpdateInfo.availableVersionCode()}\n"
-                result += "clientVersionStalenessDays() : ${appUpdateInfo.clientVersionStalenessDays()}\n"
-
-                setResultTxt(true, result)
-            }
-        }
-
-        appUpdateInfoTask.addOnFailureListener { exception ->
-            Log.e(TAG, "Failed to get app update info", exception)
-            // Exception 객체를 통해 실패 원인을 확인
-            val errorMessage = exception.message
-            val cause = exception.cause
-            Log.e(TAG, "Error message: $errorMessage")
-            Log.e(TAG, "Cause: $cause")
-
-            var result = "Error message: $errorMessage"
-            result += "\n"
-            result += "Cause: $cause"
-            setResultTxt(false, result)
-
-            Toast.makeText(this@MainActivity, "업데이트 가능여부 실패", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun setResultTxt(isSucceed: Boolean, resultText: String) {
-        val sb = StringBuilder()
-        sb.append("Update flow result: ${if (isSucceed) "SUCCESS" else "FAILED"}")
-        sb.append("\n")
-        sb.append(resultText)
-        binding.updateText.text = sb.toString()
-    }
-
-    private fun startUpdate(appUpdateInfo: AppUpdateInfo) {
-        appUpdateManager.startUpdateFlowForResult(
-            // Pass the intent that is returned by 'getAppUpdateInfo()'.
-            appUpdateInfo,
-            // an activity result launcher registered via registerForActivityResult
-            activityResultLauncher,
-            // Or pass 'AppUpdateType.FLEXIBLE' to newBuilder() for
-            // flexible updates.
-            AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build())
-
-
-        // Create a listener to track request state updates.
-        val listener = InstallStateUpdatedListener { state ->
-            // (Optional) Provide a download progress bar.
-            if (state.installStatus() == InstallStatus.DOWNLOADING) {
-                val bytesDownloaded = state.bytesDownloaded()
-                val totalBytesToDownload = state.totalBytesToDownload()
-                // Show update progress bar.
-            }
-            // Log state or install the update.
-        }
-
-        // Before starting an update, register a listener for updates.
-        appUpdateManager.registerListener(listener)
-        // Start an update.
-        // When status updates are no longer needed, unregister the listener.
-        appUpdateManager.unregisterListener(listener)
-    }
-
-
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        menuInflater.inflate(R.menu.menu_main, menu)
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        return when (item.itemId) {
-//            R.id.action_settings -> true
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//    }
-//
-//    override fun onSupportNavigateUp(): Boolean {
-//        val navController = findNavController(R.id.nav_host_fragment_content_main)
-//        return navController.navigateUp(appBarConfiguration)
-//                || super.onSupportNavigateUp()
-//    }
 }
