31236eb290b9167182804dfdbcdd0f50b421ec5d
mrsohnbit@gmail.com <mmrsohn@gmail.com>
2025-03-28 17:59:46 +0900

    앱 버전 코드 116으로 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 35
 
-        versionCode = 1
+        versionCode = 116
         versionName = "0.1.${defaultConfig.versionCode}"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
